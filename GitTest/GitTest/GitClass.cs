bfbea5b5ba6804abbd7cf53738ebd750d79d08b9
thufek <55378930+thufek@users.noreply.github.com>
2019-09-16 11:14:45 +0200

    tralalalala

---
+++ GitTest/GitTest/GitClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest
+{
+    class GitClass
+    {
+        public int GitInt { get; set; }
+    }
+}
